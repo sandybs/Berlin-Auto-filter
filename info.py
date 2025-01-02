ff2d07d0a744dc082eae51bb59c5f03f0f2a421a
Mobarak4693 <mobarakmobarakyt@gmail.com>
2025-01-02 18:58:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,8 +60,8 @@ LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "ben
 QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
 YEARS = [f'{i}' for i in range(2024 , 2002,-1 )]
 SEASONS = [f'season {i}'for i in range (1 , 23)]
-REF_PREMIUM = 
-PREMIUM_POINT = 
+REF_PREMIUM = None
+PREMIUM_POINT = None
 #---------------------------------------------------------------
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
