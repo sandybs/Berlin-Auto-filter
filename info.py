33574cc9ec7a6f6b02e5726f2f99e24393fa6d75
Mobarak4693 <mobarakmobarakyt@gmail.com>
2025-01-02 19:42:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mobarakmk4693:mobarak4693@@2A3A@cluster0.jcfqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Jail_LinkzZ")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mobarak4693@@2A3A")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
